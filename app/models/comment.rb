e0d859ef8082f67dfd607c4a5477f909b91f9ba9
Rashad-Muntar <muntartoure@gmail.com>
2021-03-24 18:01:39 +0000

    add comment model and validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
-class Post < ApplicationRecord
+class Comment < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
-
   belongs_to :user
-  has_many :comments
+  belongs_to :post
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     validates :email, presence: true 
 
     has_many :posts
+    has_many :comments
 end
