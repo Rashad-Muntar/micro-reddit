e20591677ab9daf955fc950c03ef1265d20b2620
Diego <diegodsha@hotmail.com>
2021-03-24 11:00:34 -0600

    Add Post model and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true, length: {minimum: 6, message: "You password shouldn't be less than 6 characters"}
     validates :email, presence: true 
+
+    has_many :posts
 end
