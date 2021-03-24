6bef095e9ae7d9ab28c44a16ceef85c593c154b4
Rashad-Muntar <muntartoure@gmail.com>
2021-03-24 16:24:31 +0000

    Add user model and validation

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, length: {minimum: 6, message: "You password shouldn't be less than 6 characters"}
+    validates :email, presence: true 
+end
